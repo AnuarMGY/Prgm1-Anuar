9303628f3ea5e36bea253339807e7c116b494dde
AnuarMGY <93169999+AnuarMGY@users.noreply.github.com>
2021-11-03 14:21:56 -0400

    Add files via upload
    tarea

---
+++ Circunferencia.py
@@ -0,0 +1,6 @@
+radio = input("Introduzca el radio de la circunferencia: ")
+radio = float(radio)
+longitud = float(2 * 3.1416 * radio)
+area = float(radio * radio) * 3.1416
+print("La longitud del circulo es de: ", longitud)
+print("El area del circulo es de: ", area)
---
+++ Triangulo.py
@@ -0,0 +1,6 @@
+base = input("Ingrese la base del triangulo: ")
+base = float(base)
+altura = input("Ingrese la altura del triangulo: ")
+altura = float(altura)
+superficie = float(base * altura) / 2
+print("La superficie del triangulo es de: ", superficie)
